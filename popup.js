417ee1fc1d65d8a63b3eda8d8a955a94f5160091
psinghal20 <psinghal20@gmail.com>
2017-09-10 17:32:37 +0530

    Added basic functionality

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
-window.onload= function() {
 
+window.onload= function() {
+	try{
+		updateDOM();
+		checkForUpdates();
+	}
+	catch(e){
+		console.log(e);
+	}
 	document.getElementById("form").onsubmit = function(e) {
 		e.preventDefault();
 		var tvshow=document.getElementById('tvm').value;
@@ -7,17 +14,50 @@ window.onload= function() {
 		var episode=document.getElementById('epi').value;
 		var quality=document.getElementById("qua").value;
 		var jsonObject = {
-			"tv":tvshow,
-			"sea":season,
-			"epi":episode,
-			"qua":quality
-			};
-		// Store this information in a local storage subject to access
-		let previousData = window.localStorage.getItem('jsonArray');
+			"name":tvshow,
+			"season":season,
+			"episode":episode,
+			"quality":quality
+		};
+		
+		var previousData = window.localStorage.getItem('jsonArray');
         window.localStorage.setItem('jsonArray', previousData + "++" + JSON.stringify(jsonObject));
-		console.log({"myArray": window.localStorage.getItem("jsonArray").split("++")})
-
-        
-        
+		console.log({"myArray": window.localStorage.getItem("jsonArray").split("++")});
+		updateDOM();
+		checkForUpdates();
 	}
+
+}
+
+function updateDOM(){
+	var Shows = window.localStorage.getItem("jsonArray").split("++");
+	var list = document.getElementsByTagName("ul")[0];
+	for (var i = 1; i < Shows.length; i++) {
+		var node = document.createElement("LI");
+		var textNode = document.createTextNode(JSON.parse(Shows[i]).name);
+		node.appendChild(textNode);
+		list.appendChild(node);
+	}
+}
+
+function checkForUpdates(){
+	var Shows = window.localStorage.getItem("jsonArray").split("++");
+	var show = JSON.parse(Shows[Shows.length-1]);
+	
+	console.log(show);
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST","http://localhost:8000/search/",true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	
+	xhttp.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+    		if(this.response.success == 1){
+    			alert("Torrent Found");
+    			document.getElementById("#response").innerHTML = this.response.SearchInfo.name + " Torrent found";
+    		}
+  		}
+	};
+	
+	xhttp.send("name="+show.name+"&season="+show.season+"&episode="+show.episode+"&quality="+show.quality);
 }
