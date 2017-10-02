76941abc07a764da6cf1275dfc43bd1455546ba2
psinghal20 <psinghal20@gmail.com>
2017-10-02 19:05:02 +0530

    Add CRUD operations

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 window.onload= function() {
 	try{
 		updateDOM();
@@ -7,7 +6,10 @@ window.onload= function() {
 	catch(e){
 		console.log(e);
 	}
-	document.getElementById("form").onsubmit = function(e) {
+	document.getElementById("form").onsubmit = addShow;
+}
+
+function addShow(e){
 		e.preventDefault();
 		var tvshow=document.getElementById('tvm').value;
 		var season=document.getElementById('ses').value;
@@ -20,43 +22,67 @@ window.onload= function() {
 			"quality":quality
 		};
 		
-		var previousData = window.localStorage.getItem('jsonArray');
-        if(previousData)
-        	window.localStorage.setItem('jsonArray', previousData + "++" + JSON.stringify(jsonObject));
-		else{
-			window.localStorage.setItem('jsonArray', JSON.stringify(jsonObject));	
-		}
-		console.log(createJSONobject());
+		var data = createJSONobject();
+		data.push(jsonObject);
+		storeJsonObject(data);
 		updateDOM();
 		checkForUpdates();
-	}
-
 }
 
 function createJSONobject(){
 	var shows = [];
-	var myArray = window.localStorage.getItem("jsonArray").split("++");
-	for (var i = 0; i < myArray.length; i++) {
-		shows[i] = JSON.parse(myArray[i]);
+	if(window.localStorage.getItem("jsonArray")){
+		var myArray = window.localStorage.getItem("jsonArray").split("++");
+		for (var i = 0; i < myArray.length; i++) {
+			shows[i] = JSON.parse(myArray[i]);
+		}
 	}
 	return shows;
+}
+
+function storeJsonObject(shows){
+	var string = "";
+	for (var i = 0; i<shows.length; i++) {
+		if(i!=0){
+			string = string + '++' + JSON.stringify(shows[i]);
+		}
+		else{
+			string = JSON.stringify(shows[i]);
+		}
+	}
+	window.localStorage.setItem('jsonArray',string);
+}
+
+function deleteShow(){
+	shows = createJSONobject();
+	shows.splice(this.id,1);
+	storeJsonObject(shows);
+	updateDOM();
 }
 
 function updateDOM(){
 	var Shows = createJSONobject();
 	var list = document.getElementsByTagName("ul")[0];
-	for (var i = 0; i < Shows.length; i++) {
+	while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
+    for (var i = 0; i < Shows.length; i++) {
 		var node = document.createElement("LI");
+		node.style.margin = '5px';
+		var img = document.createElement("IMG");
+		img.setAttribute('src','trash.png');
+		img.setAttribute('id',i);
+		img.style.float = 'right';
+		img.onclick = deleteShow;
 		var textNode = document.createTextNode(Shows[i].name);
 		node.appendChild(textNode);
+		node.appendChild(img);
 		list.appendChild(node);
 	}
 }
 
 function checkForUpdates(){
 	var show = createJSONobject();
-	
-	console.log(show);
 	var headers = new Headers({
 		'Content-type':'application/x-www-form-urlencoded'
 	});
